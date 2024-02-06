1cc499486789e66c7c44bf7a50473becf733bd43
Mehrdad Rafiee <rafiee.mehrdad@gmail.com>
2024-02-06 12:20:22 -0800

    render a list of items

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,36 @@
-import Image from "next/image";
+const data = [{
+  id: "1",
+  name: "laundry",
+  isComplete: false
+}, {
+  id: "2",
+  name: "car wash",
+  isComplete: false
+}, {
+  id: "3",
+  name: "shopping",
+  isComplete: false
+}]
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <main className="flex min-h-screen flex-col items-center lg:p-20 md:p-10 sm:p-5">
       <h1 className="text-xl">Todos List</h1>
       <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        {/* <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]"> */}
-        {/* </div> */}
+        <div className="z-10 w-full items-center justify-between font-mono text-sm">
+          <ul>
+            {data.map((todo) => {
+              return (
+                <li className="flex justify-between p-3 border-b-2 border-gray-500">
+                  <div className="flex items-center">
+                    <input id={todo.id} type="checkbox" className="mr-2" checked={todo.isComplete} />
+                    <label htmlFor={todo.id} className={todo.isComplete ? "line-through" : ""}>{todo.name}</label>
+                  </div>
+                </li>
+              )
+            })}
+          </ul>
+        </div>
       </div>
     </main>
   );
