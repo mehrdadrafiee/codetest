f2eff5024a565fcbdb5634f9d478483690b8a917
Mehrdad Rafiee <rafiee.mehrdad@gmail.com>
2024-02-06 12:57:48 -0800

    adds createTodo action

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@
 import { useState, useEffect } from "react";
 import Todo from "./components/Todo";
 import { v4 as uuidv4 } from 'uuid';
-import { fetchTodos } from "@/lib/actions/todo.actions";
+import { createTodo, fetchTodos } from "@/lib/actions/todo.actions";
 
 export type TodoProps = {
   id: string;
@@ -13,12 +13,23 @@ export type TodoProps = {
 
 export default function Home() {
   const [todos, setTodos] = useState<TodoProps[]>();
+  const [inputValue, setInputValue] = useState("");
+
+  const handleSubmit = async (e: any) => {
+    e.preventDefault();
+    try {
+      await createTodo({ id: uuidv4(), name: inputValue, isComplete: false })
+      setInputValue("");
+    } catch (error) {
+      console.error("Failed to create a todo:", error);
+    }
+  };
 
   useEffect(() => {
     fetchTodos()
       .then((data) => setTodos(data))
       .catch((error) => console.error("Error fetching todos:", error));
-  }, [todos]);
+  }, []);
 
   return (
     <main className="flex min-h-screen flex-col items-center lg:p-20 md:p-10 sm:p-5">
@@ -33,6 +44,15 @@ export default function Home() {
           :
           <p className="text-center">No todos yet.</p>
         }
+      </div>
+      <div className="flex mt-8 lg:w-1/2 md:w-full">
+        <input
+          type="text"
+          placeholder="New Todo..."
+          className="w-full p-4"
+          value={inputValue}
+          onChange={(e) => setInputValue(e.target.value)} />
+        <button disabled={!inputValue} onClick={handleSubmit} className="text-white bg-gray-800 p-3 hover:bg-black disabled:bg-gray-300 disabled:text-gray-500 disabled:border-gray-300 disabled:cursor-not-allowed">Add</button>
       </div>
     </main>
   );
--- lib/actions/todo.actions.ts
+++ lib/actions/todo.actions.ts
@@ -1,5 +1,6 @@
 "use server";
 
+import { TodoProps } from "@/app/page";
 import Todo from "../models/todo.model";
 import { connectToDB } from "../mongoose";
 
@@ -11,5 +12,15 @@ export async function fetchTodos() {
     return todos;
   } catch (error: any) {
     throw new Error(`Failed to fetch todos: ${error.message}`);
+  }
+}
+
+export async function createTodo({ id, name, isComplete = false }: TodoProps) {
+  try {
+    await connectToDB()
+    await Todo.create({ id, name, isComplete });
+
+  } catch (error: any) {
+    throw new Error(`Error creating the todo: ${error.message}`);
   }
 }
